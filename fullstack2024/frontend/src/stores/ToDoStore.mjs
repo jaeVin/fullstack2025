1c12aa6fbd45a7e0bdb604871115cabd2f71bea1
jaeVin <forotherthings298@gmail.com>
2025-01-23 08:49:02 +0000

    sad

--- fullstack2024/frontend/src/stores/ToDoStore.mjs
+++ fullstack2024/frontend/src/stores/ToDoStore.mjs
@@ -1,22 +1,28 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import { http } from '../utils/http.mjs';
 
-export const useToDo = defineStore ('todo-Store',() => {
-    todos = ref(null);
-    async function loadTodos(params) {
-            
-    },
-    async function addTodo(params) {
-            
-    },
-        async function changeToDo(params){
+export const useToDo = defineStore('todo-Store', () => {
+    const todos = ref([]);
+    const endPoint = "/todos"
 
-    },
-        async function setToDo(params){
+    async function loadTodos() {
+        ref = await http.get(endPoint)
+    }
 
-    },
-        async function deleteToDo(params){
+    async function addTodo(newTodo) {
+        ref = await http.post(endPoint, newTodo)
+    }
 
-    },
-    )
+    async function changeToDo(updatedTodo) {
+        ref = await http.put(endPoint, updatedTodo)
+    }
+
+    async function setToDo() {
+        ref = await http.(endPoint)
+    }
+
+    async function deleteToDo() {
+        ref = await http.delete(endPoint+"/${id}")
+    }
 });
